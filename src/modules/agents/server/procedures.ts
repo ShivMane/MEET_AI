8fcf72c8ac0492a033324847fcc851a4a690e653
ShivMane <shivprasadmane190@gmail.com>
2025-10-11 19:26:29 +0530

    11 Agents Form

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,16 +1,47 @@
-import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+// import { randomUUID } from "crypto"; // use built-in crypto instead of 'uuid' package
 
 import { db } from "@/db";
-
 import { agents } from "@/db/schema";
+import { agentsInsertSchema } from "../schemas";
+import { eq } from "drizzle-orm";
+import z from "zod";
 
 export const agentsRouter = createTRPCRouter({
-    getMany: baseProcedure.query(async () => {
-        const data = await db
-            .select()
-            .from(agents);
 
-
-        return data;
+    //TODO: change 'getOne' to use 'protectedProcedure' once auth is set up
+    getOne: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      const [existingAgent] = await db
+        .select()
+        .from(agents)
+        .where(eq(agents.id, input.id));
+      
+      return existingAgent;
     }),
+
+    getMany: protectedProcedure.query(async () => {
+    const existingAgents = await db
+        .select()
+        .from(agents);
+
+    return existingAgents; // return all agents as array
+}),
+
+    
+
+    create: protectedProcedure
+    .input(agentsInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+        const [createdAgent] = await db
+            .insert(agents)
+            .values({
+                ...input,
+                userId: ctx.auth.user.id,
+            })
+            .returning();
+
+        return createdAgent;
+    }),    
 });
